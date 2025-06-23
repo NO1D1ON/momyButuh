7eec2d9a1459a51e1bb0034bf2c457fb951d29be
NO1D1ON <228160012@students.uma.ac.id>
2025-06-23 11:41:02 +0700

    Tampilan Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,39 +5,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="{{ asset('assets/logo.png') }}?v=1.0">
     <title>Admin Login</title>
-    {{-- Memuat CSS dari folder public/css --}}
-    <link rel="stylesheet" href="/css/login.css">
+    
     {{-- Memuat Font Awesome untuk ikon input --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    {{-- TAMBAH INI: SweetAlert2 CSS --}}
+    
+    {{-- SweetAlert2 CSS --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+
+    {{-- Kode CSS baru untuk tampilan halaman login --}}
+    {{-- Anda bisa menghapus atau mengomentari link ke file login.css lama --}}
+    <link rel="stylesheet" href="/css/login.css">
 </head>
 <body>
-    <!-- @if ($errors->any())
-        <div>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif -->
-
-     {{-- Elemen tersembunyi untuk menyimpan pesan error Laravel --}}
-    {{-- Kita akan meng-encode pesan error ke dalam JSON string dan menyimpannya di atribut data-errors --}}
+    
+    {{-- Elemen tersembunyi untuk menyimpan pesan error Laravel --}}
     <div id="error-messages-data" style="display: none;" 
-        data-errors="{{ json_encode($errors->all()) }}">
+         data-errors="{{ json_encode($errors->all()) }}">
     </div>
 
     <div class="login-container">
-        <div class="logo-area">
-            {{-- Menggunakan asset() helper untuk gambar di Laravel --}}
-            <img src="{{ asset('assets/logo.png') }}" alt="Logo FS">
-        </div>
-
         <div class="login-box">
+            
+            {{-- Logo sekarang berada di dalam login-box --}}
+            <div class="logo-area">
+                <img src="{{ asset('assets/logo.png') }}" alt="Logo FS">
+            </div>
+
             <h2>Admin Login</h2>
-            <form action="{{ route('login') }}" method="POST"> {{-- Sesuaikan 'route('login')' dengan route login Anda --}}
+            <form action="{{ route('login') }}" method="POST" style="width: 100%;">
                 @csrf {{-- Laravel CSRF Token --}}
 
                 <div class="input-group">
@@ -53,11 +48,12 @@
             </form>
             
             <div class="forgot-password">
-                <a href="{{ route('ShowPass') }}">Lupa Kata Sandi?</a> {{-- Sesuaikan 'route('password.request')' --}}
+                <a href="{{ route('ShowPass') }}">Lupa Kata Sandi?</a>
             </div>
         </div>
     </div>
-{{-- SweetAlert2 JS --}}
+
+    {{-- SweetAlert2 JS --}}
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
 
     {{-- REFERENSIKAN FILE JS BARU ANDA --}}
